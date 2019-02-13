d332cfdd8e15694176b4e74baf308dd0df2d9ff2
Palash Jhamnani <palashjhamnani@gmail.com>
2019-02-14 00:19:13 +0530

    Add project files.

---
+++ blueprism/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\SESA384685\Desktop\test\blueprism\blueprism\blueprism\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
